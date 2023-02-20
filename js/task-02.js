fc777873aef05c477a6ac43bc37c1622fcb3c459
IrynaKyslytsia <i.kislica89@gmail.com>
2023-02-20 21:56:32 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,35 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+// ingredients.forEach(ing => {
+//   const itemEl = document.createElement('li');
+//   itemEl.classList.add('item');
+//   itemEl.textContent = ing;
+//   ingredientsEl.append(itemEl);
+// });
+
+const ingredientsList = ingredients.map(ing => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ing;
+
+  return itemEl;
+});
+
+ingredientsEl.append(...ingredientsList);
+
+
+
+
+
